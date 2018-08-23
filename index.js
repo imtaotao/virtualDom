f7a4d74ab7df1d79ff97ba4b798eac377094afe0
imtaotao <dwchentao@163.com>
2018-08-23 22:52:41 +0800

    fix: rename file name and fix code style

--- index.js
+++ index.js
@@ -2,30 +2,30 @@ import v, { h, diff, patch, create } from './src'
 
 // 1: Create a function that declares what the DOM should look like
 function render(count)  {
-    return h('div', {
-        style: {
-            textAlign: 'center',
-            lineHeight: (100 + count) + 'px',
-            border: '1px solid red',
-            width: (100 + count) + 'px',
-            height: (100 + count) + 'px'
-        }
-    }, [String(count)]);
+  return h('div', {
+    style: {
+      textAlign: 'center',
+      lineHeight: (100 + count) + 'px',
+      border: '1px solid red',
+      width: (100 + count) + 'px',
+      height: (100 + count) + 'px',
+    }
+  }, [String(count)])
 }
 
 // 2: Initialise the document
-var count = 0;      // We need some app data. Here we just store a count.
+let count = 0     // We need some app data. Here we just store a count.
 
-var tree = render(count);               // We need an initial tree
-var rootNode = create(tree);     // Create an initial root DOM node ...
-document.body.appendChild(rootNode);    // ... and it should be in the document
+const tree = render(count)         // We need an initial tree
+const rootNode = create(tree)     // Create an initial root DOM node ...
+document.body.appendChild(rootNode)    // ... and it should be in the document
 
 // 3: Wire up the update logic
 setInterval(function () {
-      count++;
+  count++
 
-      var newTree = render(count);
-      var patches = diff(tree, newTree);
-      rootNode = patch(rootNode, patches);
-      tree = newTree;
-}, 1000);
+  const newTree = render(count)
+  const patches = diff(tree, newTree)
+  rootNode = patch(rootNode, patches)
+  tree = newTree
+}, 1000)
