4a2fdb3c4d368aaea11f336de7e3bc82e382bd9a
imtaotao <dwchentao@163.com>
2018-08-23 22:48:34 +0800

    initialize

---
+++ REDEME.md
@@ -0,0 +1,31 @@
+## 一个简单的虚拟 dom 库
+仅供学习
+
+## api
+`h` 用于创建虚拟dom
++ h(tagName:string, props?:array, children?:vnode|string|number[]) : vnode
+
+`diff` 用于比对新旧 vtree
++ diff(oldTree, newTree) : patches
+
+`patch` 用于 apply patches，将变化映射到真实 dom 节点
++ patch(rootNode:HTMLElement, patches) : HTMLElement
+
+`create` 用于生产 dom 节点
++ create(vnode) : HTMLElement
+
+如果 vnode 有 elementCreated 钩子函数，那么这个 vnode 生成 dom 节点后，会调用此钩子
+
+```js
+  import { h, create } 'virtual-dom'
+
+  const vnode = h('div')
+
+  vnode.elementCreated = function (dom, vnode) {
+    // ...
+  }
+  
+  const dom = create(vnode)
+
+  document.getElementById('xx').appendChild(dom)
+```
---
+++ index.js
@@ -0,0 +1,31 @@
+import v, { h, diff, patch, create } from './src'
+
+// 1: Create a function that declares what the DOM should look like
+function render(count)  {
+    return h('div', {
+        style: {
+            textAlign: 'center',
+            lineHeight: (100 + count) + 'px',
+            border: '1px solid red',
+            width: (100 + count) + 'px',
+            height: (100 + count) + 'px'
+        }
+    }, [String(count)]);
+}
+
+// 2: Initialise the document
+var count = 0;      // We need some app data. Here we just store a count.
+
+var tree = render(count);               // We need an initial tree
+var rootNode = create(tree);     // Create an initial root DOM node ...
+document.body.appendChild(rootNode);    // ... and it should be in the document
+
+// 3: Wire up the update logic
+setInterval(function () {
+      count++;
+
+      var newTree = render(count);
+      var patches = diff(tree, newTree);
+      rootNode = patch(rootNode, patches);
+      tree = newTree;
+}, 1000);
